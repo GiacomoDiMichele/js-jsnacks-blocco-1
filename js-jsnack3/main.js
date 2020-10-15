8967807d12acdc2ff48039c03276b0a3b7944266
Giacomo <giacomo.1936@gmail.com>
2020-10-15 17:20:55 +0200

    Commit snack 3

---
+++ js-jsnack3/main.js
@@ -0,0 +1,6 @@
+//creo un ciclo per far ripetere 5 volte la stessa domanda, stampando i risultati della variabile
+for (var i = 0; i < 5; i++) {
+    var numero_inserito = parseInt(prompt("inserisci un numero"))
+    console.log(numero_inserito)
+}
+t 
