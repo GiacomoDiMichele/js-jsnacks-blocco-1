1128eab0205a556fe385dc2dfdc450c0325ff799
Giacomo <giacomo.1936@gmail.com>
2020-10-15 19:44:45 +0200

    modifica algoritmo

--- js-jsnack3/main.js
+++ js-jsnack3/main.js
@@ -1,6 +1,13 @@
 //creo un ciclo per far ripetere 5 volte la stessa domanda, stampando i risultati della variabile
+var somma = 0;
 for (var i = 0; i < 5; i++) {
     var numero_inserito = parseInt(prompt("inserisci un numero"))
-    console.log(numero_inserito)
+    //console.log(numero_inserito)
+    //somma = numero_inserito + somma
+    console.log(somma)
+    somma = numero_inserito + somma
+    // somma += numero_inserito
+    console.log(somma)
 }
-t 
+
+//console.log(somma)
